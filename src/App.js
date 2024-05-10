53ae22cb28804f6f63c4c608f7fc4d9fbc26f11b
AtulLohani <atull.evision@gmail.com>
2024-05-10 14:57:30 +0530

    practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, {useState,useEffect} from 'react';
 import './App.css';
 
+
 function App() {
+  const [data,setData]=useState([])
+  useEffect(()=>{
+  fetch ("https://jsonplaceholder.typicode.com/photos").then((result)=>{
+    result.json().then((resp)=>{
+      setData(resp)
+    })
+  })
+},[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>GET API CALL </h1>
+      <table border={1}>
+        <tr>
+          <td>Album ID</td>
+          <td>Album</td>
+          <td>Title</td>
+          <td>URL</td>
+          <td>Thumbnail URL</td>
+        </tr>
+        {
+          data.map((item)=>
+          <tr>
+            <td>{item.albumId}</td>
+            <td>{item.id}</td>
+            <td>{item.title}</td>
+            <td>{item.url}</td>
+            <td>{item.thumbnailUrl}</td>
+          </tr>
+          )
+        }
+      </table>
     </div>
   );
 }
-
 export default App;
